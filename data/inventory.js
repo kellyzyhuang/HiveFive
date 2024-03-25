1fe89f0821fed3b248b2152e1fb4ebf8017d25a4
Kelly Huang <kellyziyanhuang@gmail.com>
2024-03-25 16:03:26 -0700

    create quiz data and quiz main page

---
+++ data/inventory.js
@@ -0,0 +1,34 @@
+export const quiz = {
+  questions: [
+    {
+      id: 1, 
+      question: "How many different bee species are there?",
+      options: ["200", "2000", "20,000", "25,000"], 
+      answer: "25,000", 
+    },
+    {
+      id: 2, 
+      question: "How fast can a honeybee fly?",
+      options: ["5km/h", "10km/h", "24km/h", "20km/h"], 
+      answer: "24km/h", 
+    },
+    {
+      id: 3, 
+      question: "On average, how much honey does a honey bee worker produce in her lifetime?",
+      options: ["1 cup", "1/12 tablespoon", "1 teaspoon", "1/12 teaspoon"], 
+      answer: "1/12 teaspoon", 
+    },
+    {
+      id: 4, 
+      question: "How many flower visits does it take to make one pound of honey?",
+      options: ["200", "4000", "2,000,000", "200,000"], 
+      answer: "2,000,000", 
+    },
+    {
+      id: 5, 
+      question: "How many hours do honeybees sleep a night?",
+      options: ["0", "3", "8", "12"], 
+      answer: "0", 
+    }
+  ]
+}
