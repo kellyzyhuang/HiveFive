1635f2cba75e9a2bd5e7b7b6e515a1e5c6f04032
Kelly Huang <kellyziyanhuang@gmail.com>
2024-03-25 16:04:02 -0700

    update quiz main page

--- pages/Quiz.js
+++ pages/Quiz.js
@@ -1,2 +1,11 @@
 import { useState } from "react";
 import { quiz } from './data/inventory.js'; 
+import Header from "@/components/Header/index.js";
+
+export default function Quiz() {
+  return(
+    <>
+    
+    </>
+  )
+}
