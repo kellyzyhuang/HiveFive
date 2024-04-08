f9f6e0e0295ad104785ce78fb05f3635d1d14832
Arabella Alegria <a.bella@Arabellas-MacBook-Air.local>
2024-04-08 11:29:47 -0700

    wrap texts in div

--- pages/Landing.js
+++ pages/Landing.js
@@ -5,12 +5,14 @@ import Button from "@/components/buttons";
 export default function Landing(){
     return(
         <div className={styles.Landing}>
-            <Images className={styles.beehiveImg} src={"/images/beehive2.svg"} width={320} height={200}/>
+            <Images className={styles.beehiveImg} src={"/images/beehive2.svg"} width={420} height={200}/>
             <Images className={styles.leftCloud} src={"/images/left_cloud.svg"} width={200} height={100}/>
             <Images className={styles.rightCloud} src={"/images/right_cloud.svg"} width={200} height={100}/>
             <Images className={styles.happyBeenard} src={"/images/happy_bee.svg"} width={200} height={200}/>
-            <h1>HiveFive</h1>
-            <h3>Help save the bees.</h3>
+            <div className={styles.text}>
+                <h1>HiveFive</h1>
+                <h3>Help save the bees.</h3>
+            </div>
             <Images className={styles.grassImg} src={"/images/grass_bottom.svg"} width={430} height={300}/>
             <button className={styles.startButton}>Start Buzzin'</button>
         </div>
