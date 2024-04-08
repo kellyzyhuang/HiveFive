c35c5349cb2309d9ac7f23f57ad3b3e7037f2462
Arabella Alegria <a.bella@Arabellas-Air.vc.shawcable.net>
2024-04-07 20:18:20 -0700

    change background color, change butotn style

--- components/QuizResults/index.js
+++ components/QuizResults/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from "./QuizResults.module.css";
 import Header from "@/components/Header/index.js";
+import NavBar from '../NavBar';
 
 export default function QuizResults({ correctAnswersTotal, questionCount, onRestart }) {
   return (
     <div className={styles.resultScreen}>
-      <Header title="Quiz Complete!" />
+      <Header title="Results" />
       <div className={styles.scoreContainer}>
         <h1>Complete!</h1>
         <div className={styles.scoreCircle}>
@@ -16,6 +17,7 @@ export default function QuizResults({ correctAnswersTotal, questionCount, onRest
         <button className={styles.anotherQuizButton} onClick={onRestart}>Take Another Quiz</button>
         <button className={styles.homeButton} onClick={onRestart}>Return Home</button>
       </div>
+      <NavBar/>
     </div>
   );
 }
