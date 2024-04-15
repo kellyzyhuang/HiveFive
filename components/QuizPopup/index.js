3d1e40caedc15238c7975ca81d6305cf03ba831e
Kelly Huang <kellyziyanhuang@gmail.com>
2024-04-15 00:32:19 -0700

    update quiz styling

--- components/QuizPopup/index.js
+++ components/QuizPopup/index.js
@@ -1,18 +1,20 @@
-import styles from './QuizPopup.module.css'; 
+import styles from "./QuizPopup.module.css";
 
 export default function QuizPopup({ isCorrect, answer, onNextQuestion }) {
-  
+  const popupContentClass = isCorrect ? "correctBackground" : "incorrectBackground";
+
   return (
-    <div className={styles.popup}>
-      <div className={styles.popupContent}>
+    <div className={styles.popup}>  
+      <div className={`${styles.popupContent} ${styles[popupContentClass]}`}>
         <span onClick={onNextQuestion}></span>
         <p className={isCorrect ? styles.correct : styles.incorrect}>
           {isCorrect ? 'Correct!' : 'Incorrect!'}
         </p>
-        <p>{isCorrect ? '' : `The correct answer is: ${answer}`}</p>
+        <p className={styles.correctAnswer}>{isCorrect ? '' : `The correct answer is ${answer}!`}</p>
         <button className={styles.nextButton} onClick={onNextQuestion}>Next</button>
       </div>
     </div>
   );
 }
+
 
