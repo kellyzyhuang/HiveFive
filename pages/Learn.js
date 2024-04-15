ad7c9981c6a64f78223419794616eb57767016d8
Kelly Huang <kellyziyanhuang@gmail.com>
2024-04-15 01:29:00 -0700

    style articles slightly

--- pages/Learn.js
+++ pages/Learn.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Header from "@/components/Header";
 import NavBar from '@/components/NavBar';
-import styles from '@/styles/Learn.module.css'
+import styles from '@/styles/Learn.module.css';
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Learn() {
     const [data, setData] = useState([]);
-    const [description, setDescription] = useState([]);
-    const [title, setTitle] = useState([]);
-    const [author, setAuthor] = useState([]);
     
     var apiKeyInfo = process.env.NEXT_PUBLIC_API_KEY;
     var url = `https://newsapi.org/v2/everything?q=bee%20AND%20(environment%20OR%20ecology%20OR%20pollination%20OR%20"habitat%20loss")%20AND%20NOT%20(sports%20OR%20entertainment%20OR%20technology%20OR%20finance%20OR%20trump)&language=en&apiKey=${apiKeyInfo}`;
@@ -19,11 +16,7 @@ export default function Learn() {
       fetch(url)
         .then((res) => res.json())
         .then((info) => {
-          setData(info.articles)
-          console.log(info.articles);
-          console.log(info.description);
-          console.log(info.title);
-          console.log(info.author);
+          setData(info.articles);
         })
     }, [])
   
@@ -33,21 +26,22 @@ export default function Learn() {
             <Header title="Learn"/>
         <div className={styles.screen}>
             <div className={styles.articles}>
-            <div className={styles.buffer}></div>
-            {
-                data.map((i, index) => {
-                return(
-                    <div key={index}>
-                    {i.title}
-                    {i.image}
+                {data.map((article, index) => (
+                    <div key={index} className={styles.articleCard}>
+                        <div className={styles.overlay}></div>
+                        <img src='/images/bee-image3.jpg' alt="Article Image" layout='fill' objectFit='cover' />
+                        <div className={styles.date}>
+                            {new Date(article.publishedAt).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+                        </div>
+                        <div className={styles.title}>
+                            {article.title}
+                        </div>
                     </div>
-                )
-                })
-            }
+                ))}
             </div>
         </div>
         <NavBar className={styles.nav}/>
         </main>
       </>
     );
-  }
+}
