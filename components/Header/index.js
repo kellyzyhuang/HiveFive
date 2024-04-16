b5cbf0edebc57d98b8a6672116b559029387b40c
Melissa Jacobi <miffyjacobi@gmail.com>
2024-04-15 23:55:14 -0700

    HTML for hamburger menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image'; 
 import Link from 'next/link';
 
 export default function Header({ title }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  
   return(
     <header className={styles.header}>
-      {/* <Image src="/images/bee.png" alt="bee icon" width={1} height={1}/>  */}
-      {/* exit button will be here */}
       <ul className={styles.list}>
         <li className={styles.listItem}>
           <div className={styles.counter}>123</div>
@@ -15,9 +20,25 @@ export default function Header({ title }) {
           <h1 className={styles.pageTitle}>{title}</h1>
         </li>
         <li className={styles.listItem}>
-          <button className={styles.settings}><Link href="/Settings">Settings</Link></button>
+          <button className={styles.navIcon} onClick={toggleMenu}>
+            <Image src="/images/Hamburger.svg" width={60} height={60} alt="Hamburger icon"/>
+          </button>
         </li>
       </ul>
+      {isMenuOpen && (
+      <div className={styles.overlay}>
+        <div className={styles.menu}>
+          <Image  className={styles.exit} onClick={toggleMenu} src="/images/x.svg" width={20} height={20} alt="x icon"/>
+          <Link href="/menu-item-1" className={styles.menuItem}>Profile</Link>
+          <Link href="/menu-item-2" className={styles.menuItem}>settings</Link>
+          <Link href="/menu-item-3" className={styles.menuItem}>Mini Adventure</Link>
+          <Link href="/menu-item-3" className={styles.menuItem}>Acheivments</Link>
+          <Link href="/menu-item-3" className={styles.menuItem}>About</Link>
+        </div>
+      </div>
+        
+      )}
+
     </header>
   )
 }
