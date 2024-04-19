345232dfe9716be5a2e0ebffeed1209025b6c2a3
Kelly Huang <kellyziyanhuang@gmail.com>
2024-04-19 02:19:17 -0700

    edit learn more

--- pages/Learn.js
+++ pages/Learn.js
@@ -6,7 +6,7 @@ import Head from "next/head";
 import Link from "next/link";
 
 export default function Learn() {
-    const [data, setData] = useState([]);
+    const [data, setData] = useState();
     
     var apiKeyInfo = process.env.NEXT_PUBLIC_NAME;
     var url = `https://newsapi.org/v2/everything?q=bee%20AND%20(environment%20OR%20ecology%20OR%20pollination%20OR%20"habitat%20loss")%20AND%20NOT%20(sports%20OR%20entertainment%20OR%20technology%20OR%20finance%20OR%20trump)&language=en&apiKey=${apiKeyInfo}`;
@@ -27,15 +27,14 @@ export default function Learn() {
           <div className={styles.screen}>
             <div className={styles.articles}>
               <div className={styles.buffer}></div>
-              {
-                  data.map((i, index) => {
-                  return(
+              <div>
+                  {data?.map((index) => (
                       <div key={index}>
                         {i.title}
                       </div>
                   )
-                  })
-              }
+                  )}
+              </div>
             </div>
           </div>
           <NavBar className={styles.nav}/>
