adb734ff090b0ce3d5819ea8e63a043c2ff2fa27
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-04-20 00:06:22 -0700

    create tutorial pages.

--- pages/Tutorial.js
+++ pages/Tutorial.js
@@ -1,22 +1,45 @@
-import { landingPages } from "@/data/data"
+import { landingPages } from "@/data/data";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Home from ".";
+import styles from "@/styles/Tutorial.module.css";
 
 export default function Tutorial(){
 
-    const [pages, LandingPages] = useState([]);
+    const [pages, setPages] = useState([]);
+    const [pageNow, setPageNow] = useState(0);
 
+    const nextPage = () => {
+        setPageNow(pageNow + 1);
+    }
+    
     useEffect(() => {
-        setPages(pagesData);
+        setPages(landingPages.tutorials);
     }, []);
+
     return(
         <>
-        <div>
-            {pages.map((page, index) => (
-                <div key={index}>
-                    <h2>{page.title}</h2>
-                    <p>{page.content}</p>
+        <main className={`${styles.main}`}>
+            <div className={styles.Landing}>
+                <div className={styles.skip}>
+                <button className={styles.skipButton}><Link href="/Home">Skip</Link></button>{/* will have to change the index page to tutorial */}
                 </div>
-            ))}
-        </div>
+                <div className={styles.heading}>
+                <h1>HiveFive</h1>
+                <h2>{pages[pageNow]?.title}</h2>
+                </div>
+                <img className={styles.leftCloud} src="/images/left_cloud.svg" alt="left cloud" width={200} height={100}/>
+                <img className={styles.rightCloud} src="/images/right_cloud.svg" alt="right cloud" width={200} height={100}/>
+                <div className={styles.dialouge}>
+                    <p>{pages[pageNow]?.description}</p>
+                </div>
+                <img className={styles.happyBee} src="/images/happy_bee.svg" alt="happy bee mascot" width={250} height={200}/>
+                <div>
+                    <button className={styles.continueButton} onClick={nextPage}>Continue</button>
+                    {/* will add next button*/}
+                </div>
+            </div>
+        </main>
         </>
     )
 }
