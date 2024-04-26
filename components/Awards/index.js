57134b5822b21b880b6c7d1ee6eb394e9285f5bf
Melissa Jacobi <miffyjacobi@gmail.com>
2024-04-26 16:22:08 -0700

    awards components

---
+++ components/Awards/index.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import styles from './Awards.module.css';
+import Image from 'next/image'; 
+import Link from 'next/link';
+import Images from 'next/image'; 
+
+export default function Awards( {itemName, requirements, image} ) {
+
+  
+  return(
+    <>
+        <div className={styles.rewardsItem}>
+            <h3 className={styles.itemName}>{ itemName }</h3>
+            <Images src={image} width={100} height={135} alt="locked" className={styles.lockedImg}/>
+            <p>{requirements}</p>
+        </div>
+    </>
+  )
+}
