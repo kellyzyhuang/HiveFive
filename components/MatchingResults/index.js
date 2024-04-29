ca0fd11daa2992c332a63dca1b6dfbca96ac8f01
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-04-28 17:42:06 -0700

    update logic

--- components/MatchingResults/index.js
+++ components/MatchingResults/index.js
@@ -1,20 +1,35 @@
 import Header from "@/components/Header/index.js";
 import NavBar from '../NavBar';
 import React from "react";
+import Link from "next/link";
 
-export default function MatchingResults(tagLine, headline, description, matchSpecies, goHome){
+export default function MatchingResults({tagLine, correct, incorrect}){
    return(
        <>
        <div>
            <Header title="Matching"/>
            <h1>{tagLine}</h1>
-           <h2>{headline}</h2>
-           <p>{description}</p>
-           <img src="/images/happy_bee.svg" alt="happy bee"/>
-           <div>
-               <buttons onClick={matchSpecies}>Match Endangered Species</buttons>
-               <buttons onClick={goHome}>Go Home</buttons>
-           </div>
+           {correct && (
+                <div>
+                    <h2>Easy BEEzy!</h2>
+                    <p>{correct}</p>
+                </div>
+            )}
+            {incorrect && (
+                <div>
+                    <h2>BUZZy Bummer!</h2>
+                    {incorrect.map((answer) => (
+                        <div key={answer.id}>
+                            <h3>{answer.headline}</h3>
+                            <p>{answer.description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div>
+                <button><Link href="/EndangeredSpecies">Match Endangered Species</Link></button>
+                <button><Link href="/">Go Home</Link></button>
+            </div>
            <NavBar/>
        </div>
        </>
