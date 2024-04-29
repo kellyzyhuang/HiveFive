0a0baba8524bdd08bf39eb0d9c97914b43a14476
Kelly Huang <kellyziyanhuang@gmail.com>
2024-04-29 12:12:43 -0700

    add navbar to activity and matching

--- pages/Activity.js
+++ pages/Activity.js
@@ -17,6 +17,9 @@ export default function Activity() {
         <Header title="Activity" backImage="/images/back.svg" link="/" />
           <div className={styles.buffer}></div>
             <div className={styles.categories}>
+              <div className={styles.heading}>
+                <h2>Track Your Progress</h2>
+              </div>
               <div className={styles.userStats}>
                 <a href="/Stats"><img src="/images/user_stats.jpg" alt="bar graph" className={styles.userStatsImage}/></a>
                 <div className={styles.label}>
