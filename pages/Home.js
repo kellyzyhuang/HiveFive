86f93c90ac1bf48b526151e0ce8326355a2dff9e
Melissa Jacobi <miffyjacobi@gmail.com>
2024-05-02 22:30:29 -0700

    something small i forget lol

--- pages/Home.js
+++ pages/Home.js
@@ -30,7 +30,7 @@
             </div>
             <div className={styles.donation}>
               <Link className={styles.donationLink} href="/"><h3>Total Donations: </h3></Link>
-              <p>CAD ${correctAnswersTotal * 5 / 100}</p>
+              <p>CAD ${(correctAnswersTotal * 5 / 100).toFixed(2)}</p>
             </div>
             <div className={styles.quizSection}>
               <div>
