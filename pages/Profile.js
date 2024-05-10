c3a66ee7518ddfdf0f76e0a4d31545e4a35a5320
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-05-09 22:09:06 -0700

    add profile account

---
+++ pages/Profile.js
@@ -0,0 +1,23 @@
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import NavBar from "@/components/NavBar";
+import styles from "@/styles/Profile.module.css";
+import Image from "next/image";
+
+export default function Profile(){
+    return(
+        <>
+            <main className={`${styles.main}`}>
+            <Header title="Profile" backImage="/images/back.svg" link="/"/>
+                <div className={styles.screen}>
+                    <div className={styles.photoName}>
+                        <Image src="/images/anika.png" alt="woman with glasses" width={170} height={115}/>
+                        <h3>Hello, Anika!</h3>
+                    </div>
+                    <Form/>
+                </div>
+                <NavBar/>
+            </main>
+        </>
+    )
+}
