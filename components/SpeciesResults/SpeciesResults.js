0b5bcde7c882f26583728ed8ee0f95f3fa6252d3
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-05-11 22:51:30 -0700

    Add and style species page

---
+++ components/SpeciesResults/SpeciesResults.js
@@ -0,0 +1,30 @@
+import styles from "./SpeciesResults.module.css";
+import React from "react";
+import Header from "@/components/Header";
+import { speciesAnswers } from "@/data/spciesInventory";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SpeciesResults({ selectedSpecies }) {
+  const result = speciesAnswers.answers.find((species) => species.name === selectedSpecies);
+
+  return (
+    <>
+    <main className={`${styles.main}`}>
+      <Header title="Matching" backImage="/images/back.svg" link="/" />
+      <div className={styles.screen}>
+        {result && (
+          <div className={styles.result}>
+            <h1>{result.tagline}</h1>
+            <p>{result.headline}</p>
+            <p>{result.description}</p>
+            <Image src={result.photo} alt={result.alt} width={166} height={160}/>
+          </div>
+        )}
+      </div>
+      <button className={styles.plants}><Link href="/Species">Match Plants</Link></button>
+      <button className={styles.tryAgain}><Link href="/Matching">Try Again</Link></button>
+    </main>
+    </>
+  );
+}
---
+++ data/spciesInventory.js
@@ -0,0 +1,58 @@
+export const speciesAnswers = {
+    answers: [
+        {
+            name: "Bombus_lapidarius",
+            image: "/images/bombus_lapidarius.jpeg",
+            alt: "A close up of Bombus lapidarius also known as the red-tailed bumblebee",
+            tagline: "BUZZy Bummer!",
+            photo: "/images/sad_bee.svg",
+            headline: "That is a Bombus lapidarius (otherwise known as the red-tailed bumblebee).",
+            description:"Fortunately this species is not regarded as scarce or threatened and they currently thrive in the countryside and woodlands."
+        },
+        {
+            name: "Osmia_megachilidae",
+            image: "/images/mason_bee.jpeg",
+            alt: "A close up of Osmia megachilidae also known as a Mason bee on a flower.",
+            tagline: "BUZZy Bummer!",
+            photo: "/images/sad_bee.svg",
+            headline: "That is a Osmia megachilidae (otherwise known as the Mason bee).",
+            description:"Fortunately this species is not regarded as scarce or threatened and they can be found in cavities within rocks or old nail holes."
+        },
+        {
+            name: "Apis_mellifera",
+            image: "/images/western_bee.jpeg",
+            alt: "A close up of Apis mellifera also known as a Western honeybee on a flower.",
+            tagline: "BUZZy Bummer!",
+            photo: "/images/sad_bee.svg",
+            headline: "That is an Apis mellifera (otherwise known as the Western honey bee).",
+            description:"Fortunately this species is not regarded as scarce or threatened and they are the most common of the 7-12 honey bee species worldwide."
+        },
+        {
+            name: "Anthophora_plumipes",
+            image: "/images/hairy_bee.jpeg",
+            alt: "A close up of Anthophora plumipes also known as a hair footed flower bee on a flower.",
+            tagline: "BUZZy Bummer!",
+            photo: "/images/sad_bee.svg",
+            headline: "That is an Anthophora plumipes (otherwise known as the hairy footed flower bee).",
+            description:"Fortunately this species is not regarded as scarce or threatened and they can be found in much of England and Wales."
+        },
+        {
+            name: "Andrena_fulva",
+            image: "/images/tawny_bee.jpeg",
+            alt: "A close up of Andrena fulva also known as a Tawny mining bee on a flower.",
+            tagline: "BUZZy Bummer!",
+            photo: "/images/sad_bee.svg",
+            headline: "That is an Andrena fulva (otherwise known as the Tawny mining bee).",
+            description:"Fortunately this species is not regarded as scarce or threatened and they can be found in southern Britain."
+        },
+        {
+            name: "Western_bumblebee",
+            image: "/images/western_bumble.jpeg",
+            alt: "A close up of Andrena fulva also known as a Tawny mining bee on a flower.",
+            tagline: "Easy BEEzy!",
+            photo: "/images/sad_bee.svg",
+            headline: "Congratulations!",
+            description:"You are correct! Due to intensive residential and commercial development, these teddy bear-like bees have lost their habitats."
+        }
+    ]
+}
