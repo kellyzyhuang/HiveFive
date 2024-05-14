766151c8880c4fd5d07c032aa8f381e62b2685a1
Kelly Huang <kellyziyanhuang@gmail.com>
2024-05-13 23:00:49 -0700

    add landing page animation and music

--- pages/Landing.js
+++ pages/Landing.js
@@ -1,22 +1,23 @@
-import styles from "@/styles/Landing.module.css";
-import Images from "next/image";
-import Button from "@/components/buttons";
-import Tutorial from "./Tutorial";
+import Image from "next/image";
 import Link from "next/link";
+import styles from "@/styles/Landing.module.css";
 
-export default function Landing(){
-    return(
+export default function Landing() {
+
+    return (
         <div className={styles.Landing}>
-            <Images className={styles.beehiveImg} src={"/images/beehive2.svg"} width={420} height={200}/>
-            <Images className={styles.leftCloud} src={"/images/left_cloud.svg"} width={200} height={100}/>
-            <Images className={styles.rightCloud} src={"/images/right_cloud.svg"} width={200} height={100}/>
-            <Images className={styles.happyBeenard} src={"/images/happy_bee.svg"} width={200} height={200}/>
+            <Image className={styles.beehiveImg} src="/images/beehive2.svg" width={420} height={200} alt="Beehive"/>
+            <Image className={styles.leftCloud} src="/images/left_cloud.svg" width={200} height={100} alt="Left Cloud"/>
+            <Image className={styles.rightCloud} src="/images/right_cloud.svg" width={200} height={100} alt="Right Cloud"/>
+            <Image className={styles.happyBeenard} src="/images/happy_bee.svg" width={200} height={200} alt="Happy Bee"/>
             <div className={styles.text}>
                 <h1>HiveFive</h1>
                 <h3>Help save the bees.</h3>
             </div>
-            <Images className={styles.grassImg} src={"/images/grass_bottom.svg"} width={430} height={300}/>
-            <button className={styles.startButton}><Link className={styles.link} href="/Tutorial"/>Start Buzzin'</button>
+            <Image className={styles.grassImg} src="/images/grass_bottom.svg" width={430} height={300} alt="Grass"/>
+            <Link href="/Tutorial">
+                <button className={styles.startButton}>Start Buzzin'</button>
+            </Link>
         </div>
-    )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,25 @@ import Images from "next/image";
 import Button from "@/components/buttons";
 import Tutorial from "./Tutorial";
 import Link from "next/link";
+import { useRef } from 'react';
 
 export default function Landing(){
+
+    const audioRef = useRef(null);
+
+    const playMusic = () => {
+        audioRef.current.play();
+    };
+
     return(
         <div className={styles.Landing}>
+            <audio ref={audioRef} src="/music/Settings_BGM.mp3" loop/>
             <Images className={styles.beehiveImg} src={"/images/beehive2.svg"} width={420} height={200}/>
             <Images className={styles.leftCloud} src={"/images/left_cloud.svg"} width={200} height={100}/>
             <Images className={styles.rightCloud} src={"/images/right_cloud.svg"} width={200} height={100}/>
             <Images className={styles.happyBeenard} src={"/images/happy_bee.svg"} width={200} height={200}/>
             <div className={styles.text}>
-                <h1>HiveFive</h1>
+                <h1 onClick={playMusic}>HiveFive</h1>
                 <h3>Help save the bees.</h3>
             </div>
             <Images className={styles.grassImg} src={"/images/grass_bottom.svg"} width={430} height={300}/>
