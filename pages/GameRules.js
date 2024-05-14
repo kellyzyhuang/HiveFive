6431394662dffbb5cbfa10fda92620737e33378a
Kelly Huang <kellyziyanhuang@gmail.com>
2024-05-13 18:07:01 -0700

    adjust game rules

--- pages/GameRules.js
+++ pages/GameRules.js
@@ -13,12 +13,17 @@ export default function GameRules() {
                 <Header title="Mini Adveture" backImage="/images/back.svg" link="/" />
                 <div className={styles.screen}>
                     <div className={styles.buffer}></div>
-                    <Image className={styles.beenardSad} src="/images/sad_beenard.png" width={150} height={150} alt="sad beenard"/>
-                    <p className={styles.background}>
-                    Oh no! Beenard the Bee seems to have lost his way.
-                    use the WASD keys to help him navigate through this maze so he can get home!
-                    </p>
-                    <button className={styles.goButton}><Link className={styles.link} href="/Game">Let's Go!</Link></button>
+                        <div className={styles.rules}>
+                            <Image className={styles.beenardSad} src="/images/sad_beenard.png" width={170} height={150} alt="sad beenard"/>
+                            <p className={styles.background}>Oh no! Beenard the Bee seems to have gotten lost on his way! Can you help him get back home?</p>
+                            <Image src="/images/WASD.svg" alt="wasd keys" width={150} height={150}/>
+                            <p className={styles.background}>Use the WASD keys to help him navigate through the streets. </p>
+                            <Image src="/images/ArrowKeys.svg" alt="arrow keys" width={150} height={150}/>
+                            <p className={styles.background}>Use the arrow keys to pan your view.</p>
+                            <Image src="/images/Space.svg" alt="space bar" width={200} height={100}/>
+                            <p className={styles.background}>Press the space bar to jump.</p>
+                            <button className={styles.goButton}><Link className={styles.link} href="/Game">Let's Go!</Link></button>
+                        </div>
                 </div>
                 <NavBar className={styles.nav}/>
             </main>
